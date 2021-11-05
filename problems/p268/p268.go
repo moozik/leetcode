eae62188912063115f69af8f322c18cc5d1c1d52
王禹森 <wangyusen@xiaomi.com>
2021-11-05 18:00:19 +0800

    p268

---
+++ problems/p268/p268.go
@@ -0,0 +1,13 @@
+package p268
+
+//https://leetcode-cn.com/problems/missing-number/
+
+func missingNumber(nums []int) int {
+	n := len(nums)
+	sum := 0
+	for _, item := range nums {
+		sum += item
+	}
+	return (1 + n) * n / 2 - sum
+}
+
